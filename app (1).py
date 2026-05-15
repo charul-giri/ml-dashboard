667692f9f8f28bd3c82146991c0817ee4e58a35b
Charul <charulgiri28@gmail.com>
2026-05-15 12:46:48 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -20,7 +20,7 @@ from sklearn.metrics import (
     mean_absolute_error, mean_squared_error, r2_score
 )
 
-st.set_page_config(page_title="ML Dashboard", page_icon=, layout="wide")
+st.set_page_config(page_title="ML Dashboard", layout="wide")
 
 st.markdown("""
 <style>
